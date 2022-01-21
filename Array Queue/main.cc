41c8525b8e6e0f3efc663ce102c4b9b24f21c79d
Demetrios <d.lambropoulos91@gmail.com>
2022-01-20 22:44:40 -0500

    Finished array based queue

--- Array Queue/main.cc
+++ Array Queue/main.cc
@@ -2,7 +2,19 @@
 #include <iostream>
 
 int main ( int argc, char ** argv ) {
-    queue<int> q;
+    queue<int> q (4);
+
+    q.enqueue(2);
+    q.enqueue(5);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(1);
+
+    std::cout << q.dequeue() << std::endl;
+    std::cout << q.dequeue() << std::endl;
+    std::cout << q.dequeue() << std::endl;
+    std::cout << q.dequeue() << std::endl;
+    std::cout << q.dequeue() << std::endl;
 
     return 0;
 }
--- Array Queue/queue.hpp
+++ Array Queue/queue.hpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 template <typename T> 
 class queue
 {
@@ -5,20 +7,51 @@ class queue
         T * q;
         int maxSize;
         int size;
+        int read;
+        int write;
+
     public:
         queue () {
             maxSize = 16;
             q = new int[maxSize];
-            size = 0;
+            read = write = size = 0;
         }
 
         queue ( int maxSize ) {
             this->maxSize = maxSize;
             q = new int[maxSize];
-            size = 0;
+            read = write = size = 0;
         }
 
         bool empty () {
             return !size;
+        }
+
+        bool full () {
+            return size == maxSize;
+        }
+
+        void enqueue ( T value ) {
+            if (maxSize == size) {
+                std::cout << "ERROR: Queue is full." << std::endl;
+                return;
+            }
+
+            q[write] = value;
+            write = (write+1)%maxSize;
+            size++;
+            return;
+        }
+
+        T dequeue () {
+            if (!size) {
+                std::cout << "ERROR: Queue is empty." << std::endl;
+                return 0;
+            }
+
+            T val = q[read];
+            read = (read+1)%maxSize;
+            size--;
+            return val;
         }
 };
